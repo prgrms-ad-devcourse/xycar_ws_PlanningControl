8734ccebd306cf4a0a2e22f01a6aa66799628998
프로그래머스 교육팀 <learn@programmers.co.kr>
2024-03-22 23:27:26 +0900

    Add files via upload

---
+++ include/LaneKeepingSystem/PIDController.hpp
@@ -0,0 +1,21 @@
+#ifndef PID_CONTROLLER_HPP_
+#define PID_CONTROLLER_HPP_
+
+#include <cstdint>
+#include <memory>
+
+namespace Xycar {
+/**
+ * @brief PID Controller Class
+ * @tparam PREC Precision of data
+ */
+template <typename PREC>
+class PIDController
+{
+public:
+
+private:
+
+};
+} // namespace Xycar
+#endif // PID_CONTROLLER_HPP_
---
+++ src/LaneKeepingSystem/PIDController.cpp
@@ -0,0 +1,8 @@
+#include "LaneKeepingSystem/PIDController.hpp"
+namespace Xycar {
+
+
+
+template class PIDController<float>;
+template class PIDController<double>;
+} // namespace Xycar
